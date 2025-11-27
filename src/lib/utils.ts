9baddbb5e5480cf3eb68351b4ea62065a681d6d2
Waltercc25 <waltercc25@users.noreply.github.com>
2025-11-27 11:34:42 -0600

    test: agregar documentación JSDoc a función cn para probar SonarCloud

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ * Combina clases de CSS usando clsx y tailwind-merge
+ * Útil para combinar clases condicionales de Tailwind CSS
+ * @param inputs - Clases CSS a combinar
+ * @returns String con las clases combinadas y optimizadas
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
